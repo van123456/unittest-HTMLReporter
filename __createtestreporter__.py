f139ca31a7acfbd5cf86baaf26676eeaf1478837
Van Xie <2572881705@qq.com>
2018-09-20 11:40:24 +0800

    2018-09-20

---
+++ __createtestreporter__.py
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/env python
+# -*-coding:utf-8-*-
+# createtestreporter.py
+import sys
+
+sys.path.append(".")
+import HTMLTestRunner_PY3
+
+
+def createreporter(object):
+    # 打开一个文件--open一个对象，wb就是byte-write，没有会创建
+    fp = open(r"C:\Users\xiewenhua\Desktop\test.html", 'wb')
+    runner = HTMLTestRunner_PY3.HTMLTestRunner(stream=fp, title='Mobile TEST Reporter[移动测试报告]',
+                                               description='description:专业移动测试30年、专业的移动测试')
+    runner.run(object)
+
+
+if __name__ == '__main__':
+    createreporter()
---
+++ __van_unittest_py3__.py
@@ -0,0 +1,27 @@
+﻿import unittest, sys
+
+sys.path.append(r"C:\Users\xiewenhua\Desktop\test")
+sys.path.append(".")
+import HTMLTestRunner_PY3
+import __createtestreporter__
+import io
+
+
+class Mytest(unittest.TestCase):
+    def setUp(self):
+        print("这是van的测试报告")
+
+    def tearDown(self):
+        print("执行完毕")
+
+    def test_1(self):
+        '''你是一只gui'''
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    # unittest.main()
+    # test.main()
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(Mytest('test_1'))
+    __createtestreporter__.createreporter(object=test_suite)
